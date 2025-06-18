fda0f28e167fe4652c0c780d9bb93a9badce5f75
Poojitha368 <poojithareddy2662004@gmail.com>
2025-06-18 22:09:49 +0530

    added flash messages

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,38 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,flash
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login',methods = ['GET', 'POST'])
 def login():
-    return render_template("login.html",boolean=True)
-
+    data = request.form
+    print(data['email'])
+    print(data['password'])
+    return render_template("login.html")
 @auth.route('/logout')
 def logout():
     return render_template("logout.html")
 
-@auth.route('/signup')
+@auth.route('/sign-up', methods = ['GET', 'POST' ])
 def signup():
+    if request.method == 'POST':
+        user_data = request.form
+        email = user_data['email']
+        password = user_data['password']
+
+        if len(email) < 4:
+            flash("Email must be greater than 4 chars", category = "error")
+        else:
+            flash("Account created",category = "success")
+
     return render_template("signup.html")
+
+
+
+
+
+
+
+
+
+
+
