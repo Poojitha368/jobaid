1bc3832597b23458d3c18c6b372bee04b38705e8
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-06-20 00:04:39 +0530

    login manager

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "jobaid"
@@ -11,6 +13,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'sredtrfygtuyhioijk'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:password@localhost/{DB_NAME}'
     db.init_app(app)
+
     
     from .views import views
     from .auth import auth
@@ -18,4 +21,30 @@ def create_app():
     # Register blueprints
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
+
+
+    #importing tables from models
+    from .models import User
+    # database setup
+    create_database(app)
+
+
+    #where does flask redirect us when we are not logged in
+    #So as we mentioned login_required so we are saying
+    #that if not logged in go to login route in auth.py
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    #use this function to load the user with the help of id(primary key)
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+
+def create_database(app):
+    with app.app_context():
+        db.create_all()
+        print('created tables in database')
