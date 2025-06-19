c1f22ff44ce7d3ad87f67a4b522b61400185240a
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-06-19 18:36:37 +0530

    initialized db

--- website/__init__.py
+++ website/__init__.py
@@ -2,14 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-DB_NAME = "jobaid.db"
+DB_NAME = "jobaid"
 
 def create_app():
     app = Flask(__name__)
     
     # Load configuration
     app.config['SECRET_KEY'] = 'sredtrfygtuyhioijk'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/app'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:password@localhost/{DB_NAME}'
     db.init_app(app)
     
     from .views import views
