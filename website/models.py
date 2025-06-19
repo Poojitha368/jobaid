7e52c7a8d3453e8302bacb896ab83dbf5c41e309
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-06-20 00:04:55 +0530

    models

--- website/models.py
+++ website/models.py
@@ -2,12 +2,13 @@ from . import db
 from flask_login import UserMixin 
 from sqlalchemy.sql import func
 
-# schema of Notes table
-class Note(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(),default = func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+# # schema of Notes table
+# class Note(db.Model):
+#     id = db.Column(db.Integer,primary_key=True)
+#     data = db.Column(db.String(10000))
+#     date = db.Column(db.DateTime(),default = func.now())
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
 
 
@@ -16,5 +17,5 @@ class User(db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key = True)
     email = db.Column(db.String(150),unique = True)
     password = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    # notes = db.relationship('Note')
 
