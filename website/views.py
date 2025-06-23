93276b6932d5273a026ba71822f602ff1c0d4273
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-06-23 21:28:14 +0530

    added view_questions

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,render_template
 from flask_login import login_required,current_user
+from .models import Questions
 
 views = Blueprint('views',__name__)
 
@@ -8,5 +9,18 @@ views = Blueprint('views',__name__)
 def home():
     return render_template("home.html",user=current_user)
 
+@views.route('/view_questions')
+def view_questions():
+    questions = Questions.query.all()
+    '''q=[]
+    for question in questions:
+        q.append({
+            "q_id" : question.q_id,
+            "questions":question.question,
+            "answer":question.answer,
+            "skill":question.skill,
+            "difficulty":question.difficulty
+        })
+    return q'''
 
-
+    return render_template('view_questions.html',questions=questions)
