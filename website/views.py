fc9c688f7f547887ac41c974a13a81faabe873be
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-06-29 16:27:08 +0530

    made changes views

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint,render_template,request,flash
 from flask_login import login_required,current_user
 from .models import Questions
 
@@ -12,25 +12,37 @@ def home():
 @views.route('/view_questions',methods=['POST','GET'])
 def view_questions():
     if request.method == 'POST':
-        # skill=request.form['skill']
+        skill=request.form['skill']
+        difficulties = request.form.getlist('difficulty')
+        print(request.form)
+        # print(questions)
+
         # if request.form['easy']:
         #     easy=request.form['easy']
         # if request.form['medium']:
         #     medium=request.form['medium']
         # if request.form['hard']:
-        #     hard=request.form['hard']
-        print(request.form)
-    questions = Questions.query.all()
-    '''q=[]
-    for question in questions:
-        q.append({
-            "q_id" : question.q_id,
-            "questions":question.question,
-            "answer":question.answer,
-            "skill":question.skill,
-            "difficulty":question.difficulty
-        })
-    return q'''
+        #     hard=request.form['hard'] 
+        # print(request.form.getlist('difficulty'))
+        # questions = Questions.query.all()
+        if skill:
+            questions = Questions.query.filter(Questions.skill ==  skill,Questions.difficulty.in_(difficulties)).all()
+        else:
+            questions = Questions.query.filter(Questions.difficulty.in_(difficulties)).all()
+        
+        if not questions:
+            flash("No questions are present",category='error')
+        # print(questions)
+        '''q=[]
+        for question in questions:
+            q.append({
+                "q_id" : question.q_id,
+                "questions":question.question,
+                "answer":question.answer,
+                "skill":question.skill,
+                "difficulty":question.difficulty
+            })
+        return q'''
 
     return render_template('view_questions.html',questions=questions)
 
