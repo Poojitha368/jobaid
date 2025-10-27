fd63880fa659f523c9ff2bf511251d8a3cba206f
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-10-27 21:27:41 +0530

    added fetch sepecific interview questions

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -120,7 +120,6 @@ function renderFinalQuestions(){
     let tableHtml = `
     <table class='table'>
     <tr>
-    <th></th>
     <th>ID</th>
     <th>question</th>
     <th>answer</th>
@@ -218,22 +217,24 @@ function listAllInterviews(interviewList){
     }
     InterviewsContainer = `
     <div class="container text-center">
-            <div class="row row-cols-3">`
+    <div class="row row-cols-3">`
+
     interviewList.forEach(interview =>{
         InterviewsContainer += `
-                <div class="col mt-3">
-                    <div class="card interview-card text-center" data-i_id=${interview.I_id}>
-                        <div class="card-body">
-                            <h5 class="card-title">${interview.I_name}</h5>
-                            <a href="/edit_interview" class="btn btn-primary w-25 ">edit</a>
-                            <button class="btn btn-primary delete-interview w-20" data-i_id=${interview.I_id}>delete</button>
-                        </div>
-                    </div>
-                </div>`
-    })
-    InterviewsContainer += `
+        <div class="col mt-3">
+        <div class="card interview-card text-center" data-i_id=${interview.I_id}>
+        <div class="card-body">
+        <h5 class="card-title">${interview.I_name}</h5>
+        <a href="/edit_interview" class="btn btn-primary w-30">edit</a>
+        <button class="btn btn-primary delete-interview w-20" data-i_id=${interview.I_id}>delete</button>
+        <button class="btn btn-primary view-interview w-20" data-i_id=${interview.I_id}>view</button>
+        </div>
         </div>
         </div>`
+    })
+    InterviewsContainer += `
+    </div>
+    </div>`
     $('#interviews-container').html(InterviewsContainer);
 }
 
@@ -255,24 +256,69 @@ $(document).on('click','.delete-interview',function(){
     })
 })
 
-$(document).on('click','.interview-card',function(){
+$(document).on('click','.view-interview',function(){
     I_id = $(this).data('i_id');
-    viewId = {
-        "vId":I_id
+    console.log(I_id);
+    // viewId = {
+        //     "vId":I_id
+        // }
+        $.ajax({
+            url:'/view_interview',
+            type:'GET',
+            contentType:'application/json',
+            // data:JSON.stringify(viewId),
+            success:function(response){
+                console.log(response);   
+                window.location.href = '/view_interview';
+                
+                $('#view-interview-questions').html('HELLO');
+            }
+        })
+    })
+    
+function FetchSpecificInterviewQuestions(){
+    I_id = $(this).data('i_id');
+    I_id = {
+        "I_id":I_id
     }
     $.ajax({
-        url:'/view_interview',
-        type:'POST',
+        url:'/fetch_interview_questions',
+        type:'GET',
         contentType:'application/json',
-        data:JSON.stringify(viewId),
+        data:JSON.stringify(I_id),
         success:function(response){
             console.log(response);
-            window.location.href = '/view_interview'
+            DisplaySpecificInterviewQuestions(response)
         }
     })
-})
 
-function DisplaySpecificInterviewQuestions(InterviewData){
-    $('#view-interview-questions').html('HELLO');
-    
+function DisplaySpecificInterviewQuestions(InterviewQuestions){
+    if(InterviewQuestions.length==0){
+        $('#view-interview-questions').html("no questions");
+        return;
+    }
+    let tableHtml = `
+    <table class='table'>
+    <tr>
+    <th></th>
+    <th>ID</th>
+    <th>question</th>
+    <th>answer</th>
+    <th>skill</th>
+    <th>difficulty</th>
+    </tr> `
+    sno = 1
+    InterviewQuestions.forEach(I =>{
+        tableHtml += `
+        <td>${sno}</td>
+        <td>${escapeHtml(I.I_name)}</td>
+        <td>${escapeHtml(I.questions)}</td>
+        </tr>
+        `
+        sno+=1
+    })
+
+    tableHtml += `</table>`;
+    $('#view-interview-questions').html(tableHtml);
+    }
 }
