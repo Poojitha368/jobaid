2457125cfb63202ac1c890390e47ec4fa6b3fcc7
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-10-29 21:18:53 +0530

    added ai interview url direction

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     if (window.location.pathname == "/view_final_questions"){
         renderFinalQuestions();
     }
-    if(window.location.pathname == '/view_interview'){
+    if(window.location.pathname == '/view_interview' || window.location.pathname == '/ai_interview'){
         let I_id = sessionStorage.getItem("I_id");
         if(I_id){
             FetchSpecificInterviewQuestions(I_id);
@@ -12,5 +12,6 @@ $(document).ready(function(){
         }
     }
 })
+
 
 
