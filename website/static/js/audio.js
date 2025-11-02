b74416276be1ad09c1785f9defc796051a5cbbb1
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-11-02 10:54:43 +0530

    vosk recognition model

--- website/static/js/audio.js
+++ website/static/js/audio.js
@@ -12,11 +12,16 @@ function setupAudioRecording(){
                 audioChunks.push(event.data);
             };
 
-            mediaRecorder.onstop = ()=>{
+            mediaRecorder.onstop = async()=>{
                 const audioBlob = new Blob(audioChunks, {type: 'audio/wav'});
+                const base64Audio = await blobToBase64(audioBlob);
+                console.log(base64Audio);
                 const audioUrl = URL.createObjectURL(audioBlob);
+
                 console.log(audioUrl);
                 $('#audio-playback').attr('src', audioUrl);
+
+                convertToText(base64Audio);
             };
             
             $('#startRecord').prop('disabled', true);
@@ -32,5 +37,32 @@ function setupAudioRecording(){
         mediaRecorder.stop();
         $('#startRecord').prop('disabled', false);
         $('#stopRecord').prop('disabled', true);
+    })
+}
+
+function blobToBase64(blob){
+    // asynchronus
+    return new Promise((resolve,reject) =>{
+        const reader = new FileReader();
+        reader.onloadend = ()=> resolve(reader.result.split(',')[1]); // remove "data:audio/wav;base64,  from data:audio/wav;base64,UklGRigAAABXQVZFZm10IBAAAAABAAEA..."
+        reader.onerror = reject;
+        reader.readAsDataURL(blob);
+
+    });
+}
+
+
+function convertToText(base64Audio){
+    audioData = {
+        audioData : base64Audio
+    }
+    $.ajax({
+        url : '/convert_audio_to_text',
+        type: 'POST',
+        contentType : 'application/json',
+        data : JSON.stringify(audioData),
+        success : function(response){
+            console.log(response);
+        }
     })
 }
