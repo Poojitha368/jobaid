679f9d584bef16c008866bf63701a3768f2ce7a0
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-11-03 17:14:40 +0530

    displaying the received audio text from user in table

--- website/static/js/performance_scores.js
+++ website/static/js/performance_scores.js
@@ -1,4 +1,31 @@
 function PerformanceScores(){
-    questions = sessionStorage.getItem("questions")
+    questions = JSON.parse(sessionStorage.getItem("questions"));
+    answers = JSON.parse(sessionStorage.getItem("answers"));
     console.log(questions);
+    console.log(answers);
+    function showInTable(){
+        let tableHtml = `
+        <table class='table'>
+        <tr>
+        <th>Sno</th>
+        <th>question</th>
+        <th>answer</th>
+        <th>Your answer</th>
+        <th>Scores</th>
+        </tr> `
+        questions.forEach((q,index) =>{
+            tableHtml += `
+            <td>${index+1}</td>
+            <td>${escapeHtml(q.question)}</td>
+            <td>${escapeHtml(q.answer)}</td>
+            <td>${answers[index]}</td>
+            <td>10</td>
+            </tr>
+            `
+        })
+
+        tableHtml += `</table>`;
+        $('#performance-scores').html(tableHtml);
+        }
+    showInTable();
 }
