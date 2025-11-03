0add7a088ef8962051d1ec41721013efe8666a06
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-11-03 14:34:35 +0530

    added performace scores url code

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
-    if (window.location.pathname == "/view_final_questions"){
-        renderFinalQuestions();
-    }
+    let I_id = sessionStorage.getItem("I_id");
     if(window.location.pathname == '/view_interview' || window.location.pathname == '/ai_interview'){
-        let I_id = sessionStorage.getItem("I_id");
         if(I_id){
             FetchSpecificInterviewQuestions(I_id);
         }
         else{
             $('view-interview-questions').html("No inteview selected");
         }
+    }
+    if (window.location.pathname == "/view_final_questions"){
+        renderFinalQuestions();
+    }
+    if (window.location.pathname == "/performance_scores"){
+        PerformanceScores();
     }
 })
 
